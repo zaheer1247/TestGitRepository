12daad69e0b5e4e638b038ffa42d46064398df17
Zaheer.Abbas <Zaheer.Abbas@ccubefintech.com>
2017-09-30 13:03:11 +0530

    Third commit

---
+++ demo/src/hADF/TestApplitoolsWebsite2.java
@@ -0,0 +1,51 @@
+package hADF;
+
+	import com.applitools.eyes.Eyes;
+	import com.applitools.eyes.RectangleSize;
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.firefox.FirefoxDriver;
+	import java.net.URISyntaxException;
+
+	public class TestApplitoolsWebsite {
+
+	    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+ 
+	        WebDriver driver = new FirefoxDriver();
+
+	        Eyes eyes = new Eyes();
+	        // This is your api key, make sure you use it in all your tests.
+	        eyes.setApiKey("6EHAorseVX2j4mK40zk527sBACqEKpzqTIA79y105oQiw110");
+
+	        try {
+	            // Start visual testing with browser viewport set to 1024x768.
+	            // Make sure to use the returned driver from this point on.
+	            driver = eyes.open(driver, "Applitools", "Test Web Page", new RectangleSize(1024, 768));
+
+	            driver.get("http://applitools.com");
+
+	            // Visual validation point #1
+	            eyes.checkWindow("Main Page");
+
+	            driver.findElement(By.cssSelector(".features>a")).click();
+
+	            // Visual validation point #2
+	            eyes.checkWindow("Features page");
+
+	            // End visual testing. Validate visual correctness.
+	            eyes.close();
+	        } finally {
+	            // Abort test in case of an unexpected error.
+	            eyes.abortIfNotClosed();
+	            driver.close();
+	            
+	            //CTest1
+	            
+	    //change        
+	            
+	        }
+	    }
+	    
+	 
+//nhiugy
+}
